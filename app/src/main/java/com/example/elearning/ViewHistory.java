2eadeb579614f9369435ea07caf11a8acd83e6fe
ThaiPB2510 <nguyenhongthai2510@gmail.com>
2022-05-01 16:29:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/elearning/ViewHistory.java
@@ -0,0 +1,13 @@
+package com.example.elearning;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewHistory extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history_test);
+    }
+}
